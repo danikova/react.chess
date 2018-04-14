06ca8e39d00fb2c138e274f7a40625488b26610d
danikova <kovacs.daniel.9509@gmail.com>
2018-04-14 17:48:13 +0200

    KnightStepsCalculate

--- src/App/Chess/Figure/Figures/Knight.js
+++ src/App/Chess/Figure/Figures/Knight.js
@@ -18,7 +18,19 @@ class WhiteKnight extends White{
 }
 
 var _getValidSteps = function(figureAttrs, boardData){
-    console.log(figureAttrs.color, 'Knight');
+    var validSteps = initValidSteps(boardData);
+    for (let _dir = 0; _dir <= 1; _dir++) {
+        for (let _s1 = -1; _s1 <= 1; _s1+=2) {
+            for (let _s2 = -2; _s2 <= 2; _s2+=4) {
+                var bX = figureAttrs.pos.x + (_dir?_s1:_s2);
+                var bY = figureAttrs.pos.y + (_dir?_s2:_s1);
+                if(field.isEmpty(boardData, bX, bY) || field.isNotEmptyAndNotSame(boardData, bX, bY, figureAttrs)){
+                    validSteps[bY][bX] = true;
+                }
+            }
+        }
+    }
+    return validSteps;
 }
 
 export { BlackKnight, WhiteKnight };
